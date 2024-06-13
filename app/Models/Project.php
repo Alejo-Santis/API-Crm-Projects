33a0333e9f622d69c066997b528edd7a9bcdfbe6
Alejandro Santis <alejandro.santis@esinergia.co>
2024-06-12 21:12:50 -0500

    Creation of migration, models and controllers.

---
+++ app/Models/Project.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'deadline',
+        'status',
+        'user_id',
+        'client_id',
+    ];
+
+    public const STATUS = [
+        'Open',
+        'In Progress',
+        'Cancelled',
+        'Completed'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class)->withDefault();
+    }
+
+    public function client()
+    {
+        return $this->belongsTo(Client::class)->withDefault();
+    }
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
+
+    protected function deadline(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => Carbon::createFromFormat('Y-m-d', $value)->format('d/m/Y'),
+            set: fn ($value) => Carbon::createFromFormat('d/m/Y', $value)->format('Y-m-d'),
+        );
+    }
+}
